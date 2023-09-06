0484653bf292a4c4a96d637797bd2ea6b415e5ee
Gino804 <giacomo.p03@gmail.com>
2023-09-06 15:07:54 +0200

    Added relations in project and type models

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -10,4 +10,9 @@ class Project extends Model
     use HasFactory;
 
     protected $fillable = ['title', 'slug', 'description', 'image'];
+
+    public function type()
+    {
+        return $this->belongsTo(Type::class);
+    }
 }
