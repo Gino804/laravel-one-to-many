6eda2af259e957159b7b407a301792b6d9a7db97
Gino804 <giacomo.p03@gmail.com>
2023-09-06 15:02:06 +0200

    Added foreign key in project

--- database/migrations/2023_08_31_135620_create_projects_table.php
+++ database/migrations/2023_08_31_135620_create_projects_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('type_id')->constrained();
             $table->string('title', 50)->unique();
             $table->string('slug')->unique();
             $table->text('description');
